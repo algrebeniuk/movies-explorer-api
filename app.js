ab1fc82bd58a2d2336d8ee34583fd4f6de3f0f50
Alexander Grebeniuk <al.al.grebeniuk@gmail.com>
2023-02-17 10:50:45 +0300

    helmet и rate-limiter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import { set, connect } from 'mongoose';
+import helmet from 'helmet';
 import { requestLogger, errorLogger } from './middlewares/logger';
 import auth from './middlewares/auth';
 import { login, createUser } from './controllers/user';
@@ -8,6 +9,7 @@ import userRouter from './routes/user';
 import movieRouter from './routes/movie';
 import CentralizedErrorHandling from './middlewares/centralized-error-handling';
 import NotFoundError from './errors/not-found-error';
+import limiter from './middlewares/rate-limiter';
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -16,6 +18,8 @@ set('strictQuery', false);
 connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.use(json());
+app.use(limiter);
+app.use(helmet());
 app.use(requestLogger);
 
 app.post('/signup', validationOfUserSignUp, createUser);
